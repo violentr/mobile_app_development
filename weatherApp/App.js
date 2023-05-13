a3ee32fc0f77dffc347145dfda6db6184a686909
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2023-05-13 17:59:34 +0100

    Add get user permissions for his current location

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import * as Location from 'expo-location';
 import Loading from './Loading';
 
@@ -8,10 +8,18 @@ import Loading from './Loading';
 export default class extends React.Component{
 
   getLocation = async () => {
-    let options = {}
-    await Location.getCurrentPositionAsync(options);
-    console.log(location)
+    try {
+      const response = await Location.requestBackgroundPermissionsAsync();
+      console.log(response);
+      let options = {}
+      await Location.getCurrentPositionAsync(options);
+      console.log(location)
+    }catch (error) {
+      Alert.alert("Can't get weather data for now .. ")
+    }
+
   }
+
 
   componentDidMount(){
     this.getLocation();
