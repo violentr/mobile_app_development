9ef111824f61e575a7bf6b9fdbdebc57f2f3f3a1
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2023-05-13 17:52:24 +0100

    Add getLocation for user

--- weatherApp/Loading.js
+++ weatherApp/Loading.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function loading(){
     return (<View style={styles.container}>
-    <Text style={styles.text}> Receiving weather data .. </Text>
+    <Text style={styles.text}> Receiving weather data ..</Text>
     </View>)
 }
 
