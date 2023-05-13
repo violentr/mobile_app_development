e6951e37c7f87d60dd46b844a0304193710d0255
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2023-05-13 17:13:34 +0100

    Add Loading component with simple color styles | flex

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello there Den !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Loading />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ weatherApp/Loading.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function loading(){
+    return (<View style={styles.container}>
+    <Text style={styles.text}> Receiving weather data .. </Text>
+    </View>)
+}
+
+
+const styles  = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#FDF6AA",
+    },
+    text: {
+        color: "#2c1c2c",
+        fontSize: 30
+    }
+
+})
